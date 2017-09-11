ff50f44d7952554d0137739169f39c8625c2eb73
Zaman <aktarzaman84@gmail.com>
2017-09-11 12:05:39 +0600

    more

--- assets/js/shortcode-js/cx-portfolio.js
+++ assets/js/shortcode-js/cx-portfolio.js
@@ -5,39 +5,6 @@
 	Isotope Js for Portfolio Section
     ---------------------------------------------------------------- */
 
-    // // cache container
-    // var $isocontainer = $('.portfolio-wrapper');
-
-    // // initialize isotope
-
-    // $isocontainer.imagesLoaded(function() {
-    //     $isocontainer.isotope({
-    //         filter: "*",
-    //         animationOptions: {
-    //             duration: 750,
-    //             easing: 'linear',
-    //             queue: false
-    //         }
-    //     });
-    // });
-
-    // $(".portfolio-filter ul li").click(function() {
-    //     $(".portfolio-filter ul li").removeClass("active");
-    //     $(this).addClass("active");
-
-    //     var selector = $(this).attr('data-filter');
-    //     $isocontainer.isotope({
-    //         filter: selector,
-    //         animationOptions: {
-    //             duration: 750,
-    //             easing: 'linear',
-    //             queue: false,
-    //         },
-    //         isResizeBound: true
-    //     });
-    //     return false;
-    // }); //isotope finished
-
     var $isocontainer = $('.portfolio-wrapper');
 
     $isocontainer.imagesLoaded(function() {
@@ -60,15 +27,6 @@
         $('.portfolio-filter li').removeClass('active');
         $this.addClass('active');
     });
-
-
-
-
-
-
-
-
-
 
 
     /*--------------------------------------------------------------
