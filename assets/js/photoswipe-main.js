feec15c07bd8b0594ee481ddda648f081e4e6fb9
codexsazzad <sazzadhossian@codexin.com>
2017-09-12 15:36:57 +0600

    update photoswipe-main.js

--- assets/js/photoswipe-main.js
+++ assets/js/photoswipe-main.js
@@ -205,6 +205,6 @@ var initPhotoSwipeFromDOM = function(gallerySelector) {
 };
 
 // execute above function
-initPhotoSwipeFromDOM('.image-pop-up');
+initPhotoSwipeFromDOM('.image-pop-up, .portfolio-wrapper');
 
 
