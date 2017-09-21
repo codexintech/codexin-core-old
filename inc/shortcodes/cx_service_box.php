d0a2c2e14d44408c392e685f4938459ba6300da2
S.M. Rafiz <rafiz@codexin.com>
2017-09-21 18:12:45 +0600

    Added a new layout in Service Shortcode

--- inc/shortcodes/cx_service_box.php
+++ inc/shortcodes/cx_service_box.php
@@ -16,6 +16,7 @@ function cx_service_box_shortcode( $atts, $content = null ) {
             's_icon'        => '',
             's_image'       => '',
             'media_image'   => '',
+            'media_img'     => '',
             'image_alt' 	=> '',
             'service_title'	=> '',
             'service_desc'  => '',
@@ -119,7 +120,43 @@ function cx_service_box_shortcode( $atts, $content = null ) {
                 </div><!-- end of service-single-3 -->
             </div><!-- end of cx-service-box -->
 
-        <?php endif; //End layout - 3 ?>
+        <?php endif; //End layout - 3 
+
+        if( $layout == 4 ) :
+
+            // Retrieving user define classes
+            $classes = array( 'service-single-4 clearfix' );
+            (!empty($class)) ? $classes[] = $class : '';
+
+            // Retrieving the image url
+            $ret_img_url = retrieve_img_src( $media_img, 'rectangle-one' );
+
+            ?>
+
+            <div class="<?php echo esc_attr( implode( ' ', $master_class ) ); ?>">
+                <div class="<?php echo esc_attr( implode( ' ', $classes ) ); ?>">
+                    <div class="media-wrapper">
+                        <div class="media-image">
+                            <img src="<?php echo esc_url( $ret_img_url ); ?>" alt="<?php if( !empty( $image_alt ) ): echo esc_html( $image_alt ); else: echo esc_html( $service_title ); endif; ?>">
+                        </div>
+                        <div class="media-desc">
+                            <?php if( $icon_toggle ): ?>
+                                <div class="media-thumb">
+                                    <?php if( $s_media == 's_icon' ): ?>
+                                        <i class="<?php echo esc_attr( $icon ); ?>"></i>
+                                    <?php else: ?>
+                                        <img src="<?php echo esc_url( $ret_full_img_url ); ?>" alt="<?php if( !empty( $image_alt ) ): echo esc_html( $image_alt ); else: echo esc_html( $service_title ); endif; ?>">
+                                    <?php endif; ?>
+                                </div>
+                            <?php else: echo '<div class="no-media-thumb"></div>'; endif; ?>
+                            <h4 class="media-title"><?php echo esc_html( $service_title ); ?></h4>
+                            <p class="media-texts"><?php printf( '%s', $service_desc ) ; ?></p>
+                        </div>
+                    </div><!-- end of media-wrapper -->
+                </div><!-- end of service-single-3 -->
+            </div><!-- end of cx-service-box -->
+
+        <?php endif; //End layout - 4 ?>
 
 	<?php
     endif;
@@ -150,6 +187,7 @@ function cx_service_box_kc() {
                                     '1' => CODEXIN_CORE_ASSET_DIR . '/images/layout-img/servicebox/service-layout-1.jpg',
                                     '2' => CODEXIN_CORE_ASSET_DIR . '/images/layout-img/servicebox/service-layout-2.jpg',
                                     '3' => CODEXIN_CORE_ASSET_DIR . '/images/layout-img/servicebox/service-layout-3.jpg',
+                                    '4' => CODEXIN_CORE_ASSET_DIR . '/images/layout-img/servicebox/service-layout-4.jpg',
                                     ),
   	        					'value'			=> '1'
                             ),
@@ -164,8 +202,19 @@ function cx_service_box_kc() {
         					 ),
 
                             array(
+                                'name'          => 'media_img',
+                                'label'         => esc_html__( 'Upload Featured Service Image', 'codexin' ),
+                                'type'          => 'attach_image',
+                                'relation'      => array(
+                                    'parent'    => 'layout',
+                                    'show_when' => '4',
+                                ),
+                                'description'   => esc_html__( 'Upload Your Featured Service Image.', 'codexin' ),
+                            ),
+
+                            array(
                                 'name' 			=> 'icon_toggle',
-                                'label' 		=> esc_html__( 'Enable Service Media? ', 'codexin' ),
+                                'label' 		=> esc_html__( 'Enable Service Icon? ', 'codexin' ),
                                 'type' 			=> 'toggle',
                                 'value'			=> 'no'
                             ),
@@ -175,8 +224,8 @@ function cx_service_box_kc() {
                                 'label'         => esc_html__( 'Icon or Image? ', 'codexin' ),
                                 'type'          => 'dropdown',
                                 'options'       => array(
-                                        's_icon'    => "Icon",
-                                        's_image'   => "Image"
+                                        's_icon'    => "Icon Font",
+                                        's_image'   => "Image Icon"
                                 ),
                                 'relation'      => array(
                                     'parent'    => 'icon_toggle',
@@ -271,7 +320,7 @@ function cx_service_box_kc() {
 
   	        							'Icon' => array(
   	        								array('property' => 'color', 'label' => esc_html__('Color', 'codexin'), 'selector' => '.media-thumb i'),
-                                            array('property' => 'color', 'label' => esc_html__('Color On Hover', 'codexin'), 'selector' => '.service-single:hover i, .service-single-2:hover i, .service-single-3:hover i'),
+                                            array('property' => 'color', 'label' => esc_html__('Color On Hover', 'codexin'), 'selector' => '.service-single:hover i, .service-single-2:hover i, .service-single-3:hover i, .service-single-4:hover i'),
                                             array('property' => 'font-size', 'label' => esc_html__('Font Size', 'codexin'), 'selector' => '.media-thumb i'),
                                             array('property' => 'display', 'label' => esc_html__('Display', 'codexin'), 'selector' => '.media-wrapper'),
                                             array('property' => 'width', 'label' => esc_html__('Width', 'codexin'), 'selector' => '.media-thumb'),
@@ -280,25 +329,27 @@ function cx_service_box_kc() {
   	        								array('property' => 'margin', 'label' => esc_html__('Margin', 'codexin'), 'selector' => '.media-thumb i')
         								),
 
-                                        'Image' => array(
+                                        'Icon Image' => array(
                                             array('property' => 'width', 'label' => esc_html__('Width', 'codexin'), 'selector' => '.media-thumb img'),
                                             array('property' => 'max-width', 'label' => esc_html__('Max Width', 'codexin'), 'selector' => '.media-thumb img'),
                                             array('property' => 'height', 'label' => esc_html__('Height', 'codexin'), 'selector' => '.media-thumb img'),
+                                            array('property' => 'background-color', 'label' => esc_html__('Icon Image Background Color for Layout-4', 'codexin'), 'selector' => '.service-single-4 .media-thumb'),
+                                            array('property' => 'border-color', 'label' => esc_html__('Icon Image border Color for Layout-4 (Should be same as Background Color Above)', 'codexin'), 'selector' => '.service-single-4 .media-thumb::before'),
                                             array('property' => 'border', 'label' => esc_html__('Border', 'codexin'), 'selector' => '.media-thumb img'),
                                             array('property' => 'padding', 'label' => esc_html__('Padding', 'codexin'), 'selector' => '.media-thumb img'),
                                             array('property' => 'margin', 'label' => esc_html__('Margin', 'codexin'), 'selector' => '.media-thumb img')
                                         ),
 
   	        							'Box'	=> array(
-  	        								array('property' => 'background', 'selector' => '.service-single, .service-single-2, .service-single-3'),
-                                            array('property' => 'border', 'label' => esc_html__('Border', 'codexin'), 'selector' => '.service-single, .service-single-2, .service-single-3' ),
-  	        								array('property' => 'border', 'label' => esc_html__('Border on Hover', 'codexin'), 'selector' => '.service-single:hover, .service-single-2:hover, .service-single-3:hover' ),
-  	        								array('property' => 'border-radius', 'label' => esc_html__('Border Radius', 'codexin'), 'selector' => '.service-single, .service-single-2, .service-single-3' ),
-  	        								array('property' => 'box-shadow', 'label' => esc_html__('Box Shadow', 'codexin'), 'selector' => '.service-single, .service-single-2, .service-single-3'),
-  	        								array('property' => 'box-shadow', 'label' => esc_html__('Box Shadow on Hover', 'codexin'), 'selector' => '.service-single:hover, .service-single-2:hover, .service-single-3:hover'),
-  	        								array('property' => 'transition', 'label' => esc_html__('Hover Transition Animation', 'codexin'), 'selector' => '.service-single, .service-single-2, .service-single-3'),
-  	        								array('property' => 'margin', 'label' => esc_html__('Margin', 'codexin'), 'selector' => '.service-single, .service-single-2, .service-single-3' ),
-  	        								array('property' => 'padding', 'label' => esc_html__('Padding', 'codexin'), 'selector' => '.service-single, .service-single-2, .service-single-3' ),
+  	        								array('property' => 'background', 'selector' => '.service-single, .service-single-2, .service-single-3, .service-single-4'),
+                                            array('property' => 'border', 'label' => esc_html__('Border', 'codexin'), 'selector' => '.service-single, .service-single-2, .service-single-3, .service-single-4' ),
+  	        								array('property' => 'border', 'label' => esc_html__('Border on Hover', 'codexin'), 'selector' => '.service-single:hover, .service-single-2:hover, .service-single-3:hover, .service-single-4:hover' ),
+  	        								array('property' => 'border-radius', 'label' => esc_html__('Border Radius', 'codexin'), 'selector' => '.service-single, .service-single-2, .service-single-3, .service-single-4' ),
+  	        								array('property' => 'box-shadow', 'label' => esc_html__('Box Shadow', 'codexin'), 'selector' => '.service-single, .service-single-2, .service-single-3, .service-single-4'),
+  	        								array('property' => 'box-shadow', 'label' => esc_html__('Box Shadow on Hover', 'codexin'), 'selector' => '.service-single:hover, .service-single-2:hover, .service-single-3:hover, .service-single-4:hover'),
+  	        								array('property' => 'transition', 'label' => esc_html__('Hover Transition Animation', 'codexin'), 'selector' => '.service-single, .service-single-2, .service-single-3, .service-single-4'),
+  	        								array('property' => 'margin', 'label' => esc_html__('Margin', 'codexin'), 'selector' => '.service-single, .service-single-2, .service-single-3, .service-single-4' ),
+  	        								array('property' => 'padding', 'label' => esc_html__('Padding', 'codexin'), 'selector' => '.service-single, .service-single-2, .service-single-3, .service-single-4' ),
         								)									
         							)
         						)
