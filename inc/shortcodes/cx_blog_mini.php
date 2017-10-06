05a89baedab9c0d64dc2ae777d0bc850d47da221
S.M. Rafiz <rafiz@codexin.com>
2017-10-06 11:43:09 +0600

    Some fix

--- inc/shortcodes/cx_blog_mini.php
+++ inc/shortcodes/cx_blog_mini.php
@@ -1,14 +1,14 @@
-<?php 
+<?php
 
 
 /*
     ================================
-        CODEXIN BLOG SHORTCODE
+        CODEXIN BLOG MINI SHORTCODE
     ================================
 */
 
-// Registering Codexin Blog Shortcode
-function cx_blog_shortcode( $atts, $content = null ) {
+// Registering Codexin Blog Mini Shortcode
+function cx_blog_mini_shortcode( $atts, $content = null ) {
 	extract(shortcode_atts(array(
 			'layout'			=> '',
 			'number_of_posts'	=> '',
@@ -27,34 +27,449 @@ function cx_blog_shortcode( $atts, $content = null ) {
 
 	$result = '';
 
-	// Assigning a master css class and hooking into KC
-	$master_class = apply_filters( 'kc-el-class', $atts );
-	$master_class[] = 'cx-blog';
+	// Extracting user excluded categories
+	$cat_exclude = str_replace(',', ' ', $exclude);
+	$cat_excludes = explode( " ", $cat_exclude );
 
     // Retrieving user define classes
     $classes = array( 'row' );
     (!empty($class)) ? $classes[] = $class : '';
 
-
 	ob_start(); 
 
+	if( ! empty( $layout ) ) :
+   		if( $layout == 1 ) :
+   		// Assigning a master css class and hooking into KC
+	    $master_class = apply_filters( 'kc-el-class', $atts );
+	    $master_class[] = 'cx-blog';
+
+	?>
+		<div class="<?php echo esc_attr( implode( ' ', $master_class ) ); ?>">
+			<div class="<?php echo esc_attr( implode( ' ', $classes ) ); ?>">
+
+				<?php
+
+				//start query..
+				$args = array(
+					'post_type'				=> 'post',
+					'posts_per_page'		=> $number_of_posts,
+					'post_status'			=> 'publish',
+					'meta_key'				=> ( $orderby == 'meta_value_num' ) ? 'cx_post_views' : '',
+					'order'					=> $order,
+					'orderby'				=> $orderby,
+					'category__not_in' 		=> $cat_excludes,
+					'ignore_sticky_posts' 	=> 1
+				);
 
 
-}
+				$data = new WP_Query( $args );
+
+				if( $data->have_posts() ) :
+
+					while( $data->have_posts() ) : $data->the_post();
+					$column = 12/$number_of_posts;
+
+					// Retrieving Image alt tag
+					$image_alt = ( !empty( retrieve_alt_tag() ) ) ? retrieve_alt_tag() : get_the_title();
+
+				 ?>
+					<div class="col-md-<?php echo esc_html( $column ); ?> col-sm-<?php echo ( $column==4 ) ? esc_html($column) : esc_html(6); ?>">
+						<div class="blog-wrapper">
+							<div class="img-thumb">
+								<a href="<?php esc_url( the_permalink() ); ?>">
+									<div class="img-wrapper">
+										<img src="<?php echo esc_url( ( has_post_thumbnail() ) ? the_post_thumbnail_url( 'rectangle-one' ) : '//placehold.it/600x400' ); ?>" alt="<?php echo esc_attr( $image_alt ); ?>" class="img-responsive">
+									</div>
+								</a>
+								<?php if( $show_date ) : ?>
+								<div class="meta">
+									<p><?php echo get_the_time( 'd' ); ?></p>
+									<p><?php echo get_the_time( 'M' ); ?></p>
+								</div>
+								<?php endif; ?>
+							</div> <!-- End of img-thumb -->
+
+							<div class="blog-content">
+								<p class="blog-title">
+									<a href="<?php the_permalink(); ?>"><?php echo esc_html( wp_trim_words( get_the_title(), $title_length ) ); ?></a>
+								</p>
+								<p class="blog-desc"> 
+									<?php echo esc_html( wp_trim_words( get_the_excerpt(), $desc_length ) ); ?> 
+								</p>
+								<a class="read-more" href="<?php echo esc_url( get_the_permalink() ); ?>">
+									<?php echo esc_html( !empty( $readmore_text ) ? $readmore_text : __('Read More', 'codexin') ); ?>
+								</a>
+							</div> <!-- end of blog-content -->
+						<?php
+							if( $postview_comments ) : ?>
+							<div class="blog-info">
+								<?php
+								$order_cpv = 'meta_value_num';
+								$order_com = 'comment_count';
+								if( $orderby == $order_cpv ) : 
+								?>
+								<span>
+									<i class="fa fa-eye"></i> <i>
+										<?php echo codexin_get_post_views(get_the_ID()); ?>
+									</i>
+								</span>
+								<span> 											
+									<i class="fa fa-comments"></i> <i>
+										<a href="<?php comments_link(); ?>"><?php comments_number('0', '1', '%'); ?></a>
+									</i>
+								</span>
+								<?php elseif( $orderby == $order_com ) : ?>
+								<span> 											
+									<i class="fa fa-comments"></i> <i>
+										<a href="<?php comments_link(); ?>"><?php comments_number('0', '1', '%'); ?></a>	
+									</i>
+								</span>
+								<span>
+									<i class="fa fa-eye"></i> <i>
+										<?php echo codexin_get_post_views(get_the_ID()); ?>
+									</i>
+								</span>
+								<?php else : ?>
+								<span>
+									<i class="fa fa-eye"></i> <i>
+										<?php echo codexin_get_post_views(get_the_ID()); ?>
+									</i>
+								</span>
+								<span> 											
+									<i class="fa fa-comments"></i> <i>
+										<a href="<?php comments_link(); ?>"><?php comments_number('0', '1', '%'); ?></a>
+									</i>
+								</span>
+							<?php endif; ?>	
+
+							</div><!-- end of blog-info -->
+
+						<?php endif; ?>
+
+						</div><!--end of blog-wrapper -->
+					</div> <!-- end of col -->
+					<?php
+
+					endwhile;
+				endif;
+				wp_reset_postdata();
+				?>
+			</div> <!-- end of row -->
+		</div> <!-- end of cx-blog -->
+
+		<div class="clearfix"></div>
+
+	<?php endif; //End Layout - 1
+
+		if( $layout == 2 ) :
+		// Assigning a master css class and hooking into KC
+		$master_class = apply_filters( 'kc-el-class', $atts );
+		$master_class[] = 'cx-blog-2';
+	
+		?>
+		<div class="<?php echo esc_attr( implode( ' ', $master_class ) ); ?>">
+			<div class="<?php echo esc_attr( implode( ' ', $classes ) ); ?>">
+
+				<?php 
+				//start query..
+				$args = array(
+					'post_type'				=> 'post',
+					'posts_per_page'		=> $number_of_posts,
+					'post_status'			=> 'publish',
+					'meta_key'				=> ( $orderby == 'meta_value_num' ) ? 'cx_post_views' : '',
+					'order'					=> $order,
+					'orderby'				=> $orderby,
+					'category__not_in' 		=> $cat_excludes,
+					'ignore_sticky_posts' 	=> 1
+				);
+
+				$data = new WP_Query( $args );
+
+				if( $data->have_posts() ) :
+
+					while( $data->have_posts() ) : $data->the_post();
+					$column = 12/$number_of_posts;
+
+					// Retrieving Image alt tag
+					$image_alt = ( !empty( retrieve_alt_tag() ) ) ? retrieve_alt_tag() : get_the_title();
+
+					?>
+					<div class="col-md-<?php echo $column ?> col-sm-<?php echo ( $column==4 ) ? esc_html($column) : esc_html(6); ?>">
+						<div class="blog-wrapper">
+							<div class="img-thumb">
+								<img src="<?php echo esc_url( ( has_post_thumbnail() ) ? the_post_thumbnail_url( 'rectangle-one' ) : '//placehold.it/600x400' ); ?>" alt="<?php echo esc_attr( $image_alt ); ?>" class="img-responsive">
+								<a href="<?php the_permalink(); ?>" class="cx-blog-btn">
+									<?php echo esc_html( !empty( $readmore_text ) ? $readmore_text : __('Read More', 'codexin') ); ?>
+								</a>
+								<?php if( $show_date ) : ?>
+									<div class="date-time"> 
+										<p><?php echo get_the_time( 'd' ); ?></p>
+										<p><?php echo get_the_time( 'M' ); ?></p>
+									</div>
+								<?php endif; ?>
+							</div> <!-- End of img-thumb -->
+
+							<div class="blog-content">
+								<h3 class="blog-title">
+									<a href="<?php the_permalink(); ?>"><?php echo esc_html( wp_trim_words( get_the_title(), $title_length ) ); ?></a>
+								</h3>
+								<p class="blog-category"> <?php the_category( '  ' ); ?> </p>
+								<ul class="meta">
+									<li><i class="fa fa-user"></i> <a href="<?php echo esc_url(get_author_posts_url( get_the_author_meta( 'ID' ), get_the_author_meta( 'user_nicename' ) )); ?>"> <?php the_author(); ?> </a> </li>
+									<?php if( $post_meta == 'comment' ): ?>
+										<li><i class="fa fa-paper-plane"></i> <a href="<?php comments_link(); ?>"><?php comments_number('0', '1', '%'); ?></a> </li>
+									<?php else: ?>
+										<li><i class="fa fa-eye"></i> <?php echo codexin_get_post_views(get_the_ID()); ?> </li>
+									<?php endif; ?>
+									<li><?php if( function_exists( 'codexin_likes_button' ) ): echo codexin_likes_button( get_the_ID(), 0 ); endif; ?></li>
+								</ul>
+								<p class="blog-desc">
+									<?php echo esc_html( wp_trim_words( get_the_excerpt(), $desc_length ) ); ?> 
+								</p>
+							</div> <!-- end of blog-content -->
+						</div> <!-- end of blog-wrapper -->
+					</div> <!-- end of col -->
+					<?php
+
+					endwhile;
+				endif;
+				wp_reset_postdata();
+				?>
+			</div> <!-- end of row -->
+		</div> <!-- end of cx-blog-2 -->
+		<div class="clearfix"></div>
+
+	<?php endif; //End Layout - 2 
+	
+	if( $layout == 3 ) : 
+		// Assigning a master css class and hooking into KC
+		$master_class = apply_filters( 'kc-el-class', $atts );
+		$master_class[] = 'cx-blog-3';
+
+		$number_of_posts = 4;
+
+		?>
+			
+		<div class="<?php echo esc_attr( implode( ' ', $master_class ) ); ?>">
+			<div class="<?php echo esc_attr( implode( ' ', $classes ) ); ?>">
+			<?php 
+			//start query..
+			$args = array(
+					'post_type'				=> 'post',
+					'posts_per_page'		=> ( $sticky_post ) ? $number_of_posts-1 : 4,
+					'post_status'			=> 'publish',
+					'meta_key'				=> ( $orderby == 'meta_value_num' ) ? 'cx_post_views' : '',
+					'order'					=> $order,
+					'orderby'				=> $orderby,
+					'category__not_in' 		=> $cat_excludes,
+					'ignore_sticky_posts' 	=> ( $sticky_post ) ? '' : 1
+			);
+
+			$data = new WP_Query( $args );
+
+			if( $data->have_posts() ) :
+				$i = 1;
+
+				while( $data->have_posts() ) : $data->the_post();
+
+				// Retrieving Image alt tag
+				$image_alt = ( !empty( retrieve_alt_tag() ) ) ? retrieve_alt_tag() : get_the_title();
+				if($i == 1): 
+				?>
+					<!-- Left column post -->
+					<div class="col-md-6 col-sm-4 col-xs-12">
+						<div class="blog-wrapper-left">
+							<div class="img-thumb">
+								<a href="<?php esc_url( the_permalink() ); ?>">
+									<img src="<?php echo esc_url( ( has_post_thumbnail() ) ? the_post_thumbnail_url( 'rectangle-one' ) : '//placehold.it/600x400' ); ?>" alt="<?php echo esc_attr( $image_alt ); ?>" class="img-responsive">
+								</a>
+							</div> <!-- End of img-thumb -->
+							<div class="blog-content">
+								<h3 class="blog-title">
+									<a href="<?php the_permalink(); ?>"><?php echo esc_html( wp_trim_words( get_the_title(), $title_length ) ); ?></a>
+								</h3>
+								<ul class="meta">
+									<li><?php echo esc_html__('By', 'reveal'); ?> <a href="<?php echo esc_url(get_author_posts_url( get_the_author_meta( 'ID' ), get_the_author_meta( 'user_nicename' ) )); ?>"> <?php the_author(); ?> </a> </li>
+									<li><?php echo esc_html__('On', 'reveal'); ?> <a href="<?php echo get_day_link(get_post_time('Y'), get_post_time('m'), get_post_time('j'));  ?>" class="entry-date"><?php echo get_the_time( 'd M, Y' ); ?></a> </li>
+									<li><?php echo esc_html__('In', 'reveal'); ?> <?php the_category( ', ' ); ?> </li>
+								</ul>
+								<p class="blog-desc">
+									<?php echo esc_html( wp_trim_words( get_the_excerpt(), $desc_length ) ); ?> 
+								</p>
+							</div> <!-- end of blog-content -->
+							<div class="blog-footer">
+								<div class="blog-footer-item"><i class="fa fa-comments"></i> <a href="<?php comments_link(); ?>"><?php comments_number('0', '1', '%'); ?></a> </div>
+								<div class="blog-footer-item"><i class="fa fa-eye"></i> <?php echo codexin_get_post_views(get_the_ID()); ?> </div>
+								<a href="<?php the_permalink(); ?>" class="cx-blog-btn">
+									<?php echo esc_html( !empty( $readmore_text ) ? $readmore_text : __('Read More', 'codexin') ); ?>
+								</a>
+								<div class="clearfix"></div>
+							</div>
+						</div> <!-- end blog-wrapper-left -->
+					</div> <!--end The col -->
+
+					<!-- Right column posts -->
+					<div class="col-md-6 col-sm-8 col-xs-12">
+						<ul class="blog-wrapper-right">
+				<?php else : ?>
+							<li>
+								 <a class="thumbnail-link" href="<?php esc_url( the_permalink() ); ?>" style="background-image: url('<?php echo esc_url( ( has_post_thumbnail() ) ? the_post_thumbnail_url( 'square-one' ) : '//placehold.it/220x220' ); ?>');"> 
+									<div class="media-wrapper-right" >
+										<div class="img-thumb">
+											<img src="<?php echo esc_url( ( has_post_thumbnail() ) ? the_post_thumbnail_url( 'square-two' ) : '//placehold.it/500x500' ); ?>" alt="<?php echo esc_attr( $image_alt ); ?>" class="img-responsive">
+										</div> <!-- End of img-thumb -->
+									</div>
+								</a> 
+								<!-- End of media-wraper-right -->
+								<div class="blog-content">
+									<h3 class="blog-title">
+										<a href="<?php the_permalink(); ?>"><?php echo esc_html( wp_trim_words( get_the_title(), $title_length ) ); ?></a>
+									</h3>
+									<ul class="meta">
+										<li><?php echo esc_html__('By', 'reveal'); ?> <a href="<?php echo esc_url(get_author_posts_url( get_the_author_meta( 'ID' ), get_the_author_meta( 'user_nicename' ) )); ?>"> <?php the_author(); ?> </a> </li>
+										<li><?php echo esc_html__('On', 'reveal'); ?> <a href="<?php echo get_day_link(get_post_time('Y'), get_post_time('m'), get_post_time('j'));  ?>" class="entry-date"><?php echo get_the_time( 'd M, Y' ); ?></a> </li>
+										<li><i class="fa fa-eye"></i> <?php echo codexin_get_post_views(get_the_ID()); ?> </li>
+										<li><i class="fa fa-comments"></i> <a href="<?php comments_link(); ?>"><?php comments_number('0', '1', '%'); ?></a> </li>
+									</ul>
+									<p class="blog-desc">
+										<?php echo esc_html( wp_trim_words( get_the_excerpt(), $desc_length ) ); ?> 
+									</p>
+								</div> <!-- end of blog-content -->
+							</li>
+						<?php 
+						endif; //End if($i == 1)
+
+					if( $number_of_posts == $i ) : ?>
+						</ul> <!-- End Post blog-wrapper-right-->	
+					</div> <!-- end of col -->	
+					<?php endif; ?>
+
+					<?php 
+						$i++;
+					endwhile;
+					endif;
+					wp_reset_postdata();
+					?>
+				</div> <!-- end of row -->
+			</div> <!-- end of cx-blog-3 -->
+			<div class="clearfix"></div>
+
+	<?php endif; //End layout - 3 
+
+	if( $layout == 4 ) : 
+		// Assigning a master css class and hooking into KC
+		$master_class = apply_filters( 'kc-el-class', $atts );
+		$master_class[] = 'cx-blog-4';
+
+		$number_of_posts = 3;
+		?>
+			
+		<div class="<?php echo esc_attr( implode( ' ', $master_class ) ); ?>">
+			<div class="<?php echo esc_attr( implode( ' ', $classes ) ); ?>">
+			<?php 
+			//start query..
+			$args = array(
+					'post_type'				=> 'post',
+					'posts_per_page'		=> $number_of_posts,
+					'post_status'			=> 'publish',
+					'meta_key'				=> ( $orderby == 'meta_value_num' ) ? 'cx_post_views' : '',
+					'order'					=> $order,
+					'orderby'				=> $orderby,
+					'category__not_in' 		=> $cat_excludes,
+					'ignore_sticky_posts' 	=> 1
+			);
+
+			$data = new WP_Query( $args );
+
+			if( $data->have_posts() ) :
+
+				while( $data->have_posts() ) : $data->the_post();
+
+				// Retrieving Image alt tag
+				$image_alt = ( !empty( retrieve_alt_tag() ) ) ? retrieve_alt_tag() : get_the_title();
+				?>
+					<!-- Left column post -->
+					<div class="col-md-4 col-sm-4">
+						<div class="blog-wrapper">
+							<div class="img-thumb">
+								<a href="<?php esc_url( the_permalink() ); ?>">
+									<img src="<?php echo esc_url( ( has_post_thumbnail() ) ? the_post_thumbnail_url( 'square-two' ) : '//placehold.it/500x500' ); ?>" alt="<?php echo esc_attr( $image_alt ); ?>" class="img-responsive">
+								</a>
+							</div> <!-- End of img-thumb -->
+							<div class="blog-content">
+								<div class="blog-category">
+									<?php the_category(' '); ?>
+								</div>
+								<h3 class="blog-title">
+									<a href="<?php the_permalink(); ?>"><?php echo esc_html( wp_trim_words( get_the_title(), $title_length ) ); ?></a>
+								</h3>
+								<ul class="meta">
+									<li><i class="fa fa-clock-o"></i> <a href="<?php echo get_day_link(get_post_time('Y'), get_post_time('m'), get_post_time('j'));  ?>"><?php echo get_the_time( 'd M, Y' ); ?></a> </li>
+									<li><i class="fa fa-eye"></i> <?php echo codexin_get_post_views(get_the_ID()); ?> </li>
+									<li><i class="fa fa-comments"></i> <a href="<?php comments_link(); ?>"><?php comments_number('No Comments', '1 Comment', '% Comments'); ?></a> </li>
+								</ul>
+							</div> <!-- end of blog-content -->
+						</div> <!-- end blog-wrapper-left -->
+					</div> <!--end The col -->
+					<?php 
+					endwhile;
+				endif;
+				wp_reset_postdata();
+				?>
+				</div> <!-- end of row -->
+			</div> <!-- end of cx-blog-4 -->
+			<div class="clearfix"></div>
+
+	<?php endif; //End layout - 4 ?>
+			
+	<?php
+	endif;
+	$result .= ob_get_clean();
+	return $result;
+
+} //End of cx_blog_mini
+
+/**
+ *
+ * Helper function to fetch all post categories
+ *
+ */  
+function cx_get_post_categories() {
+
+	$categories = get_categories( array(
+	    'orderby' => 'name',
+	    'order'   => 'ASC'
+	) );
+
+	$post_cat = array();
+	if ( $categories ) {		
+
+		foreach ( $categories as $value ) {
+			$post_cat[$value->term_id] = ucwords(strtolower( $value->name ) ) . ' (Posts Count: '. $value->category_count .')';
+		}
+
+	} else {
+
+		$post_cat[0] = esc_html__( 'No Categories found', 'codexin' );
+
+	}
+
+	return $post_cat;
 
 
+} //End cx_get_post_categories()..
 
 // Integrating Shortcode with King Composer
-function cx_blog_kc() {
+function cx_blog_mini_kc() {
 
 	$cx_categories = cx_get_post_categories();
 
 	if (function_exists('kc_add_map')) { 
 		kc_add_map(
 			array(
-				'cx_blog' => array(
-					'name' => esc_html__( 'Codexin Blog', 'codexin' ),
-					'description' => esc_html__('Codexin Blog', 'codexin'),
+				'cx_blog_mini' => array(
+					'name' => esc_html__( 'Codexin Mini Blog', 'codexin' ),
+					'description' => esc_html__('Codexin Mini Blog', 'codexin'),
 					'icon' => 'et-hazardous',
 					'category' => 'Codexin',
 					'params' => array(
@@ -338,6 +753,6 @@ function cx_blog_kc() {
 			) //end of  array 
 		);  //end of kc_add_map
 	} //End if
-} // end of cx_blog_kc
+} // end of cx_blog_mini_kc
 
 
